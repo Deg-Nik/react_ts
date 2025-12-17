523942f4b0ec6b33d33e2e415c5c931c2cab8936
AIT-AS <a.schuetz79@gmail.com>
2025-12-17 16:58:08 +0100

    added_2

---
+++ src/lessons/lesson_11/styles.ts
@@ -0,0 +1,26 @@
+import styled from "@emotion/styled";
+
+export const PageWrapper = styled.div`
+display: flex;
+flex: 1;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+background-color: rgba(215, 218, 235, 0.9);
+`;
+
+export const ContainerInputButton = styled.form`
+
+`;
+
+export const Card = styled.div`
+
+`;
+
+export const Text = styled.p`
+
+`;
+
+export const ErrorText = styled.p`
+
+`;
