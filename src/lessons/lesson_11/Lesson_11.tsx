ffbf6ec6bf2679ff0f1855f85dc89c18e3d2bbcc
Nikolai <degnikolaj@gmail.com>
2025-12-17 05:31:27 +0100

    add logic to card

--- src/lessons/lesson_11/Lesson_11.tsx
+++ src/lessons/lesson_11/Lesson_11.tsx
@@ -2,11 +2,20 @@ import { useEffect, useState, type ChangeEvent } from "react";
 import axios from "axios";
 import Input from "components/Input/Input";
 import Button from "components/Button/Button";
-import { PageWrapper, ContainerInputButton, Card, Text, ErrorText } from "./styles";
+import {
+  PageWrapper,
+  ContainerInputButton,
+  Card,
+  Text,
+  ErrorText,
+} from "./styles";
+import { v4 } from "uuid";
 
 export default function Lesson_11() {
   const [country, setCountry] = useState<string>("");
-  const [output, setOutput] = useState<[]>([])
+  const [output, setOutput] = useState<
+    { country: string; name: string; web_pages: string }[]
+  >([]);
   const [error, setError] = useState<string | undefined>(undefined);
   const [isDisabled, setIsDisabled] = useState<boolean>(false);
 
@@ -16,11 +25,6 @@ export default function Lesson_11() {
     setCountry(event.target.value);
   };
 
-  // const input = (event: any) => {
-  //   event.preventDefault();
-  //   console.log(country);
-  // };
-
   const getCountry = async (event: any) => {
     event.preventDefault();
     setOutput([]);
@@ -29,20 +33,14 @@ export default function Lesson_11() {
 
     try {
       const response = await axios.get(UrlUni);
-      const data = response.data;
-      // тут пишем логику, когда нам пришла шутка(статус промиса fullfiled)
+      setOutput(response.data);
       // setOutput(`${data.country} ${data.name} ${data.web_pages}`);
     } catch (error: any) {
-      // тут пишем логику, когда нам пришла ошибка(статус промиса rejected)
       setError(error.message);
     } finally {
       setIsDisabled(false);
     }
   };
-
-  // useEffect(() => {
-  //   getCountry();
-  // }, []);
 
   return (
     <PageWrapper>
@@ -58,7 +56,13 @@ export default function Lesson_11() {
         <Button isDisabled={isDisabled} name="Get Universities" type="submit" />
       </ContainerInputButton>
       <Card>
-        {!!output && <Text>{output}</Text>}
+        {output.length > 0 &&
+          output.slice(0, 15).map((universities) => (
+            <Text key={v4()}>
+              {universities.country} {universities.name}{" "}
+              {universities.web_pages}
+            </Text>
+          ))}
         {!!error && <ErrorText>{error}</ErrorText>}
       </Card>
     </PageWrapper>
