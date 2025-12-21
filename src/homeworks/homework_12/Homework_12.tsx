943682472111226b0487a569c2f5dee26c33d9eb
Nikolai <degnikolaj@gmail.com>
2025-12-21 10:11:56 +0100

    add homework 12

---
+++ src/homeworks/homework_12/Homework_12.tsx
@@ -0,0 +1,8 @@
+import { PageWrapper } from './styles'
+import ContactUs from 'components/ContactUs/ContactUs'
+
+export default function Homework_12() {
+  return (
+    <PageWrapper><ContactUs/></PageWrapper>
+  )
+}
