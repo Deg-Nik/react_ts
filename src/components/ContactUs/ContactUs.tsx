2385cfd49adb429d24e4a96e8f9e43a4aa40a766
Nikolai <degnikolaj@gmail.com>
2025-12-23 08:05:38 +0100

    update

--- src/components/ContactUs/ContactUs.tsx
+++ src/components/ContactUs/ContactUs.tsx
@@ -17,8 +17,10 @@ export default function ContactUs() {
       .max(20, "maximum 20 simbol"),
     [CONTACT_US_VALUES.EMAIL]: Yup.string()
       .min(6, "minimum 6 simbol")
-      .max(60, "maximum 60 simbol"),
+      .max(60, "maximum 60 simbol")
       .email("invalid email format"),
+  });
+
   const formik = useFormik({
     initialValues: {
       [CONTACT_US_VALUES.FULLNAME]: "",
