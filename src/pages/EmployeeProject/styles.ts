e3b2b265d5ac2a4748cfc5c037d492a7b03c9b7c
Afina33 <megasemen33@gmail.com>
2026-01-10 19:14:11 +0100

    Обновление

--- src/pages/EmployeeProject/styles.ts
+++ src/pages/EmployeeProject/styles.ts
@@ -11,9 +11,10 @@ export const Header = styled.header`
   display: flex;
   justify-content: space-between;
   width: 100%;
-  height: 100px;
-  border-bottom: 1px solid black;
-  padding: 20px 40px;
+  height: 120px;
+  border-bottom: 2px solid black;
+  padding: 42px 84px;
+  color: black;
 `;
 
 export const Logo = styled.div`
