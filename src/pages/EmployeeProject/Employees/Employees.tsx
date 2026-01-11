91467533bddfe03ff29aded4eb5f3bdbb9f5fea1
Nikolai <degnikolaj@gmail.com>
2026-01-11 13:22:26 +0100

    add update Employees

--- src/pages/EmployeeProject/Employees/Employees.tsx
+++ src/pages/EmployeeProject/Employees/Employees.tsx
@@ -1,12 +1,19 @@
+import { useContext } from "react";
 import OutputForm from "../components/OutputForm/OutputForm";
 import { PageWrapper } from "../CreateEmployee/styles";
+import { EmployeeContext } from "../Layout";
 
 
 
 export default function Employees() {
+
+  // карточка показывается только если все поля заполнены
+  const {employeeData} = useContext(EmployeeContext)
+  const isFilled = employeeData.name && employeeData.surname && employeeData.age && employeeData.job_position;
+
   return (
     <PageWrapper>
-       <OutputForm/>
+       {isFilled && <OutputForm/>}
     </PageWrapper>
   )
 }
